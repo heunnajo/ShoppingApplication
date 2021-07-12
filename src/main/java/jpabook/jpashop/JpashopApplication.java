342b130743f91932bbf4052dcde8bd6f47d2dba3
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-12 09:08:03 +0900

    MemberRepository
    1. save()
    2. findOne()
    3. findAll()
    4. findByName()

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -3,7 +3,7 @@ package jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//하위의 컴포넌트 애노테이션들 모두 스캔한다.
 public class JpashopApplication {
 
 	public static void main(String[] args) {
