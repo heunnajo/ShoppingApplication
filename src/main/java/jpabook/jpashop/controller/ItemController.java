0f4bfc5a053cd7bab1cb0e5a6ea9c7f6b830c01d
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-12 21:27:33 +0900

    Implemented Web!!!(Controllers and Templates)
    1. Implemented Controller Get and Post Mapping
    2. Created static resources including html,css,js.

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form",new BookForm());
+        return "items/createItemForm";
+    }
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+        //setter를 다 제거하고 파라미터로 넘기는 것이 더 좋은 설계.
+        //static 생성자 메서드로.
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
